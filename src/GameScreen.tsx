a18e8f45e2d4c84e832f21ba1fcad8d7947ae1d2
Edouard Penin <e.peninb@gmail.com>
2019-04-11 13:02:17 +0200

    Displaying level from parent state

--- src/GameScreen.tsx
+++ src/GameScreen.tsx
@@ -10,13 +10,13 @@ interface GameScreenProps
 }
 
 
-export function GameScreen( { state: { targetHue, currentHue } }: GameScreenProps )
+export function GameScreen( { state: { targetHue, currentHue, level } }: GameScreenProps )
 {
 	return (
 		<div className="text-white h-screen flex flex-col">
 			<header className="flex items-center p-4">
 				<h1 className="text-4xl font-bold">Color match!</h1>
-				<p className="ml-auto text-4xl font-bold">Level 1</p>
+				<p className="ml-auto text-4xl font-bold">Level {level}</p>
 			</header>
 			<main className="text-center flex-grow flex flex-col px-2">
 				
