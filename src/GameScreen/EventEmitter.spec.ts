ec261afbb6e9fdb05ef5fafb454c83689065c96d
Edouard Penin <e.peninb@gmail.com>
2019-04-19 07:27:12 +0200

    Tests refactoring

--- src/GameScreen/EventEmitter.spec.ts
+++ src/GameScreen/EventEmitter.spec.ts
@@ -21,73 +21,76 @@ class EventEmitter
 	}
 	
 	
-	fire( event: string ): void
+	fire( event: string, payload?: any ): void
 	{
 		if ( this._listeners.has( event ) )
 			this._listeners.get( event )!
-				.forEach( l => l() )
+				.forEach( l => l( payload ) )
 	}
 }
 
 
 describe( `EventEmitter`, () => {
-	describe( `Can be subscribed to`, () => {
-		const eventAListener = jest.fn(),
-		      eventBListener = jest.fn(),
-		      emitter        = new EventEmitter()
-		
-		Given( () => {
-			emitter.on( "A", eventAListener )
-			emitter.on( "B", eventBListener )
+	let emitter: EventEmitter
+	
+	beforeEach( () => emitter = new EventEmitter() )
+	
+	Feature( `I can subscribe to an EventEmitter`, () => {
+		Scenario( `Triggering an event with subscribers`, () => {
+			const eventAListener = jest.fn(),
+			      eventBListener = jest.fn()
+			
+			Given( () => {
+				emitter.on( "A", eventAListener )
+				emitter.on( "B", eventBListener )
+			} )
+			
+			When( () => {
+				emitter.fire( "A" )
+			} )
+			
+			Then( `Only subscribers to the fired "A" event are called`, () => {
+				expect( eventAListener ).toHaveBeenCalled()
+				expect( eventBListener ).not.toHaveBeenCalled()
+			} )
 		} )
 		
-		When( () => {
-			emitter.fire( "A" )
+		Scenario( `Multiple subscribers for an event`, () => {
+			const firstListener  = jest.fn(),
+			      secondListener = jest.fn()
+			
+			Given( () => {
+				emitter.on( "blah", firstListener )
+				emitter.on( "blah", secondListener )
+			} )
+			
+			When( () => {
+				emitter.fire( "blah" )
+			} )
+			
+			Then( `Calls every subscriber to this event`, () => {
+				expect( firstListener ).toHaveBeenCalled()
+				expect( secondListener ).toHaveBeenCalled()
+			} )
 		} )
 		
-		test( `Only triggers the subscribers to the triggered event`, () => {
-			expect( eventAListener ).toHaveBeenCalled()
-			expect( eventBListener ).not.toHaveBeenCalled()
+		Scenario( `No subscribers`, () => {
+			test( `Doesn't crash`, () => {
+				expect( () => emitter.fire( "blah" ) ).not.toThrow()
+			} )
 		} )
 	} )
 	
-	describe( `Can have multiple subscribers for the same event`, () => {
-		const firstListener  = jest.fn(),
-		      secondListener = jest.fn(),
-		      emitter        = new EventEmitter()
-		
-		Given( () => {
-			emitter.on( "blah", firstListener )
-			emitter.on( "blah", secondListener )
-		} )
-		
-		When( () => {
-			emitter.fire( "blah" )
-		} )
-		
-		test( `Calls every subscriber to this event`, () => {
-			expect( firstListener ).toHaveBeenCalled()
-			expect( secondListener ).toHaveBeenCalled()
-		} )
-	} )
-	
-	describe( `An event can stop listening`, () => {
-		const listener = jest.fn(),
-		      emitter  = new EventEmitter()
+	Feature( `I can unsubscribe from an EventEmitter`, () => {
+		const listener = jest.fn()
 		
 		let unsubscribe: unsubscribeFn
 		
 		Given( () => {
 			unsubscribe = emitter.on( "blah", listener )
-
-			emitter.fire( "blah" )
-			
-			expect( listener ).toHaveBeenCalled()
-			
-			listener.mockReset()
 		} )
 		
-		test( `Event is not called anymore after unsubscribing`, () => {
+		Then( `Event is not called anymore after unsubscribing`, () => {
 			unsubscribe()
 			
 			emitter.fire( "blah" )
@@ -95,9 +98,25 @@ describe( `EventEmitter`, () => {
 			expect( listener ).not.toHaveBeenCalled()
 		} )
 	} )
-	// passing a payload
+	
+	Feature( `A payload can be passed to the event`, () => {
+		const listener = jest.fn(),
+		      payload  = "payload"
+		
+		Given( () => {
+			emitter.on( "blah", listener )
+		} )
+		
+		When( () => {
+			emitter.fire( "blah", payload )
+		} )
+		
+		Then( `Listener should be passed the payload`, () => {
+			expect( listener ).toHaveBeenCalledWith( payload )
+		} )
+	} )
+	
 	// typing/mapping event -> payload {[event: ""|""|""]: paylaod[ev]
-	// Doesn't fail when triggering events with nithing registered
 } )
 
 
